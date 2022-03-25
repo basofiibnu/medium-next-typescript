e817d8aca0f55029f72c7169277fb0a31bb6c6bf
basofiibnu <>
2022-03-25 09:41:37 +0700

    added header

---
+++ components/headers.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+
+import logo from '../assets/logo.png';
+
+const Header = () => {
+  return (
+    <header className="mx-auto flex max-w-7xl justify-between p-5">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png"
+            alt="main-logo"
+            className="w-44 cursor-pointer object-contain"
+          />
+        </Link>
+        <div className="hidden items-center space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="rounded-full border border-green-600 px-4 py-1">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
